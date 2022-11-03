3c8da26ab174e7f61798edf9e30e5d4ab51f6afb
gaurav-kumar17 <gk170106@gmail.com>
2022-11-03 09:49:16 +0530

    life cycle method useEffect

--- src/Container/Home/index.js
+++ src/Container/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import H2TagComponents from "../../Components/H2Tag";
 import HeaderComponents from "../../Components/Header";
 import FooterComponents from "../../Components/Footer";
@@ -11,6 +11,8 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const HomeContainer = () => {
+
+  // console.log("home page container rerender");
   const [number, setNumber] = useState(5);
 
   const h2tagArrayData = ["Rohit", "Abhishek", "Saurabh"];
@@ -25,6 +27,32 @@ const HomeContainer = () => {
   //     // let updateNo=number+1;
   //     setNumber(number + 1);
   //   };
+
+  // It will run only once.
+    useEffect(function(){
+      console.log("Use Effect called first time only");
+    },[])
+
+    const numberUpd=()=>{
+      console.log("return func.");
+    }
+
+    // It will run when component is updated.
+    useEffect(function(){
+      // setNumber(number+1)  donot use this it will move infinite
+      numberUpd();
+      console.log("Run on update only");
+    },[number])
+
+
+    // It will unmount using useEffect that means when we go to another component after leaving one component then it will render.
+    useEffect(function(){
+      console.log("UseEffect Run remove only");
+      return ()=>{
+          console.log("unmount use Effect")
+      }
+    },[])
+
   return (
     <>
       {/* <HeaderComponents /> */}
