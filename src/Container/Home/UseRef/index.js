bde43ec7bf9e3c289eaa7953def00fc3ac2be987
gaurav-kumar17 <gk170106@gmail.com>
2022-11-04 08:05:48 +0530

    created useRef file

---
+++ src/Container/Home/UseRef/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const useRefComp = () => {
+  return (
+    <>
+        <input type="text"/>
+        <button>Click to bottom.</button>
+    </>
+  )
+}
+
+export default useRefComp
+
