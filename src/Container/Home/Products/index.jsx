47dd299363e2bbaee14249ea2e3c718c862ebb5d
gaurav-kumar17 <gk170106@gmail.com>
2022-11-11 08:40:50 +0530

    created function to show all categories

--- src/Container/Home/Products/index.jsx
+++ src/Container/Home/Products/index.jsx
@@ -10,19 +10,30 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-
-
 import CategoryComponent from "../../../Components/Category";
 
 const ProductContainer = () => {
   const [productData, setProductData] = useState([]);
   const [productDataCat, setProductDataCat] = useState([]);
-  const [currentCat,setCurrentCat]=useState('smartphones');
+  const [currentCat, setCurrentCat] = useState();
+
+  const [inputValue,setInputValue]=useState(currentCat);
 
   // const API_URL = "https://dummyjson.com/products?limit=10";
 
   const fetchData = async () => {
-    const data = await axios(`https://dummyjson.com/products/category/${currentCat}`);
+
+    let apiUrl="";
+    if(currentCat && currentCat!=="")
+    {
+      apiUrl=`https://dummyjson.com/products/category/${currentCat}`;
+    }
+    else{
+      apiUrl=`https://dummyjson.com/products`;
+    }
+
+
+    const data = await axios(apiUrl);
     const myData = data.data.products;
     setProductData(myData);
     // console.log("data", data);
@@ -36,16 +47,34 @@ const ProductContainer = () => {
     console.log("datacat", productDataCat);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let catValue=inputValue;
+    if(productDataCat.indexOf(catValue)===-1)
+    {
+      alert("This text is not fount in category.")
+    }
+    else{
+      setCurrentCat(catValue);
+    }
+    console.log("Form Submit");
+  };
+
+  const showAllProducts=()=>{
+
+    setCurrentCat('');
+  }
+
+
   // yaha par useEffect tab run hoga jab app/total components load ho jayega.
   useEffect(() => {
     fetchData();
     fetchDataCate();
   }, []);
 
-  useEffect(()=>{
-
+  useEffect(() => {
     fetchData();
-  },[currentCat]);
+  }, [currentCat]);
 
   return (
     <>
@@ -59,25 +88,44 @@ const ProductContainer = () => {
               setCurrentCat={setCurrentCat}
               currentCat={currentCat}
             />
-            
           </Col>
 
           <Col sm={10}>
-            <select value={currentCat} onChange={(e)=>{
-
-              setCurrentCat(e.target.value);
-            }}>            
-            {
-              productDataCat && productDataCat.length > 0 ? productDataCat.map((item)=>{
-                return(
-                  <option key={item} value={item}>
-                    {item}
-                  </option>
-                )
-              }) : <option>Data Not Found!</option>
-            }
-
+            <select
+              value={currentCat}
+              onChange={(e) => {
+                setCurrentCat(e.target.value);
+              }}
+            >
+              {productDataCat && productDataCat.length > 0 ? (
+                productDataCat.map((item) => {
+                  return (
+                    <option key={item} value={item}>
+                      {item}
+                    </option>
+                  );
+                })
+              ) : (
+                <option>Data Not Found!</option>
+              )}
             </select>
+
+            <div className="">
+              <form onSubmit={handleSubmit}>
+
+                <input
+                  type="text"
+                  value={inputValue}
+                  onChange={(e) => {
+                    setInputValue(e.target.value.toLowerCase());
+                  }}
+                ></input>
+
+                <button type="submit">Search Item</button>
+              </form>
+
+              <button type="button" onClick={showAllProducts}>All Products</button>
+            </div>
 
             {productData && productData.length > 0
               ? productData.map((item) => {
@@ -95,8 +143,6 @@ const ProductContainer = () => {
                 })
               : "Data Not Found"}
           </Col>
-
-
         </Row>
       </Container>
     </>
